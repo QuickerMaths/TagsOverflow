0d2277df24f5b8f910f2a85b4f4adf2c2c1f67c9
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-03-30 01:07:36 +0100

    Add align prop to DropdownMenu component

--- src/stories/molecules/DropdownMenu.stories.tsx
+++ src/stories/molecules/DropdownMenu.stories.tsx
@@ -16,6 +16,7 @@ type CustomArgs = {
   labelChildren: string;
   itemChildren: string;
   disabled: boolean;
+  align: "start" | "end" | "center";
 };
 
 type DropdownMenuWithCustomArgs = ComponentProps<typeof DropdownMenu> &
@@ -29,6 +30,12 @@ const meta: Meta<DropdownMenuWithCustomArgs> = {
     labelChildren: { control: { type: "text" } },
     itemChildren: { control: { type: "text" } },
     disabled: { control: { type: "boolean" } },
+    align: {
+      control: {
+        type: "radio",
+      },
+      options: ["start", "end", "center"],
+    },
   },
   decorators: [
     (Story) => (
@@ -48,6 +55,7 @@ export const Default: Story = {
     labelChildren: "Dropdown Menu Label",
     itemChildren: "Dropdown Menu Item",
     disabled: true,
+    align: "center",
   } satisfies DropdownMenuWithCustomArgs,
   render: ({
     triggerChildren,
@@ -76,6 +84,7 @@ export const WithButton: Story = {
     labelChildren: "Dropdown Menu Label",
     itemChildren: "Dropdown Menu Item",
     disabled: true,
+    align: "center",
   } satisfies DropdownMenuWithCustomArgs,
   render: ({
     triggerChildren,
@@ -99,3 +108,42 @@ export const WithButton: Story = {
     </DropdownMenu>
   ),
 };
+
+import { Moon, Sun } from "lucide-react"
+
+
+export const  ModeToggle: Story = {
+  args: {
+    triggerChildren: "Dropdown Menu",
+    labelChildren: "Dropdown Menu Label",
+    itemChildren: "Dropdown Menu Item",
+    disabled: true,
+    align: "end",
+  } satisfies DropdownMenuWithCustomArgs,
+  render: ({
+    align
+  }: DropdownMenuWithCustomArgs) => {
+    return (
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" size="icon">
+            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            <span className="sr-only">Toggle theme</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align={align}>
+          <DropdownMenuItem>
+            Light
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Dark
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            System
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    )
+  }
+}
