3c6067bcdd79a10880110c769b7cc21f913ea2f2
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-03-30 15:28:10 +0100

    Add routing and layout components to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
+import { Route, Routes } from "react-router"
+import Tags from "./pages/tags"
+import Layout from "./components/layout"
+import NotFound from "./pages/not-found"
+
 function App() {
   return (
-    <></>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Tags />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './css/globals.css'
 import { ThemeProvider } from './context/theme-provider.tsx'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>,
 )
