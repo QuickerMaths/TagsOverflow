3f006ef2f973482f949ddd888b95989620684f7e
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-04-01 21:48:11 +0200

    Add useTagsUrl hook for generating tags API URL with queryparams

---
+++ src/hooks/useTagsUrl.tsx
@@ -0,0 +1,40 @@
+import { z } from 'zod'
+import { formSchema } from '@/components/form'
+
+interface TagsQueryParams {
+    formState: z.infer<typeof formSchema>
+}
+
+const API_URL = 'https://api.stackexchange.com'
+
+const useTagsUrl = ({ formState }: TagsQueryParams) => {
+    const { inname, order, pagesize, date, minMax, sort } = formState
+    const { fromdate, todate } = date
+
+    const fromDate = fromdate ? Math.floor(fromdate.getTime()/1000) : undefined
+    const toDate = todate ? Math.floor(todate.getTime()/1000) : undefined
+    const min = minMax?.min || undefined
+    const max = minMax?.max || undefined
+
+    const url = new URL(`${API_URL}/2.3/tags?page=1`)
+
+    const setParam = (key: string, value: string | number | undefined) => {
+        if (value !== undefined) {
+            url.searchParams.set(key, value.toString())
+        }
+    }
+
+    setParam('pagesize', pagesize)
+    setParam('order', order)
+    setParam('sort', sort)
+    setParam('site', 'stackoverflow')
+    setParam('inname', inname)
+    setParam('fromdate', fromDate)
+    setParam('todate', toDate)
+    setParam('min', min)
+    setParam('max', max)
+
+    return url.toString()
+}
+
+export default useTagsUrl
