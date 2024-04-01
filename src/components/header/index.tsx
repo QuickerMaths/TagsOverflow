3c7239be665599bf74916d4e6e8a9da6d112f487
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-04-01 21:47:38 +0200

    Refactor header component to use header element

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,12 +4,12 @@ import Typography from '../typography'
 
 const Header = () => {
   return (
-    <div className='w-full border-b border-primary'>
+    <header className='w-full border-b border-primary'>
         <Container className="flex-between">
             <Typography tag='h2' variant='h1'>TagsOverflow</Typography>
             <ModeToggle />
         </Container>
-    </div>
+    </header>
   )
 }
 
