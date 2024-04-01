0caa1c875f94f0f986e9596473daaafc9dc1d6db
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-04-01 21:49:04 +0200

    Update tags page with lazy loading and skeletons

--- src/pages/tags/index.tsx
+++ src/pages/tags/index.tsx
@@ -1,14 +1,23 @@
 import Container from '@/components/container'
-import Form from '@/components/form'
-import TagsProvider from '@/context/tags-context'
+import FormSkeleton from '@/components/form-skeleton'
+import Typography from '@/components/typography'
+import { Suspense, lazy } from 'react'
+import TagsListSkeleton from '@/components/tag-list-skeleton'
+
+const Form = lazy(() => import('@/components/form'))
+const TagsList = lazy(() => import('@/components/tags-list'))
 
 const Tags = () => {
   return (
     <main>
       <Container>
-        <TagsProvider>
+        <Suspense fallback={<FormSkeleton />}>
           <Form />
-        </TagsProvider>
+        </Suspense>
+        <Typography variant="h1" tag='h1' className="inline">Tags</Typography>
+        <Suspense fallback={<TagsListSkeleton />}>
+          <TagsList />
+        </Suspense>
       </Container>
     </main> 
   )
