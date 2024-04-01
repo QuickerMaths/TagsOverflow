e291c0d3bc46546e955213240784425febd412d4
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-04-01 21:47:51 +0200

    Add suspense and spinner to layout component

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from 'react-router-dom'
 import Header from '../header'
+import { Suspense } from 'react'
+import Spinner from '../spinner'
 
 const Layout = () => {
   return (
     <>
       <Header />
-      <Outlet />
+      <Suspense fallback={<Spinner className='w-[100px] h-[100px]' />}>
+        <Outlet />
+      </Suspense>
     </>
   )
 }
