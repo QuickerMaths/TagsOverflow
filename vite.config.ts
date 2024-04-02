11b1a533b11f281039e61743caf5cd1463bc7703
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-04-02 19:26:13 +0200

    Exclude 'js-big-decimal' from optimizeDeps

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ['js-big-decimal']
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
