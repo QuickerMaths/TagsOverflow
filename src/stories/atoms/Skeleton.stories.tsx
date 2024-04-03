2ea86cd887508d376227930ce8beaa8fbd12e60d
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-04-03 23:37:04 +0200

    Add Skeleton, HeaderSkeleton, FormSkeleton, and TagsList components

--- src/stories/atoms/Skeleton.stories.tsx
+++ src/stories/atoms/Skeleton.stories.tsx
@@ -1,17 +1,25 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
 import { Skeleton } from "@/components/ui/skeleton";
+import HeaderSkeleton from "@/components/header-skeleton";
+import FormSkeleton from "@/components/form-skeleton";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import TagsProvider from "@/context/tags-context";
+
 
 const meta: Meta<typeof Skeleton> = {
   component: Skeleton,
   tags: ["autodocs"],
-  decorators: [
-    (Story) => (
-      <div className="flex justify-center items-center">
-        <Story />
-      </div>
-    ),
-  ],
+  decorators: (Story) => {
+    const queryClient = new QueryClient()
+    return (
+      <QueryClientProvider client={queryClient}>
+          <TagsProvider>
+              <Story />
+          </TagsProvider>
+      </QueryClientProvider>
+    )
+}
 };
 
 export default meta;
@@ -27,4 +35,24 @@ export const Default: Story = {
       </div>
     </div>
   ),
+};
+
+export const Header: Story = {
+  render: () => <HeaderSkeleton />,
+};
+
+export const Form: Story = {
+  render: () => <FormSkeleton />,
+};
+
+export const TagsList: Story = {
+  render: () => {
+    return (
+      <ul className="w-full my-5 grid grid-cols-1 min-[400px]:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2">
+        {Array.from({ length: 10 }).map((_, i) => (
+          <Skeleton key={i} className="h-[110px] sm:h-[114px] md:h-[118px] lg:h-[122px] border border-ring " />
+        ))}
+      </ul>
+    )
+  }
 };
