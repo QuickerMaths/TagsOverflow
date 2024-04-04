5c305ec104a0065e03010c262e4479c1bd488f26
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-04-04 03:00:03 +0200

    Add react-router parameters to TagsList.stories.tsx

--- src/stories/organisms/TagsList.stories.tsx
+++ src/stories/organisms/TagsList.stories.tsx
@@ -4,23 +4,37 @@ import TagsList from '@/components/tags-list';
 import Tag from '@/components/tag';
 import TagsListSkeleton from '@/components/tag-list-skeleton';
 import Pagination from '@/components/pagination';
-import { FunctionComponent } from 'react';
+import { FunctionComponent, lazy } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import TagsProvider from '@/context/tags-context';
+import { reactRouterParameters, withRouter } from 'storybook-addon-remix-react-router';
 
-type TagsListProps = {
-};
 
-const meta: Meta<TagsListProps> = {
+
+
+const meta: Meta<typeof TagsList> = {
   component: TagsList,
   subcomponents: {
     Tag: Tag as FunctionComponent<unknown>,
     TagsListSkeleton: TagsListSkeleton as FunctionComponent<unknown>,
     Pagination: Pagination as FunctionComponent<unknown>,
   },
+  parameters: {
+    reactRouter: reactRouterParameters({
+      routing: {
+        path: '/',
+        Component: lazy(() => import('../../components/tags-list/index')),
+        loader: () => import('../../services/tagsLoader'),
+        ErrorBoundary: () => (<div>Router Error</div>),
+      }
+    }),
+  },
   tags: ['autodocs'],
-  decorators: (Story) => {
+  decorators: [
+    withRouter,
+    (Story) => {
       const queryClient = new QueryClient()
+
       return (
         <QueryClientProvider client={queryClient}>
             <TagsProvider>
@@ -28,11 +42,11 @@ const meta: Meta<TagsListProps> = {
             </TagsProvider>
         </QueryClientProvider>
       )
-  }
+  }]
 };
 
 export default meta;
-type Story = StoryObj<TagsListProps>;
+type Story = StoryObj<typeof TagsList>;
 
 export const Default: Story = {
   render: () => {
