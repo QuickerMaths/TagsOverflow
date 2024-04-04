9ef7a5bc0fb8fed55d5d8a92a90fbafdc3fa6682
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-04-04 02:58:44 +0200

    Add react-router parameters to Tags template

--- src/stories/templates/Tags.stories.tsx
+++ src/stories/templates/Tags.stories.tsx
@@ -3,20 +3,44 @@ import { Meta, StoryObj } from '@storybook/react'
 import Tags from '@/pages/tags'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import TagsProvider from '@/context/tags-context'
+import { reactRouterParameters, withRouter } from 'storybook-addon-remix-react-router'
+import { lazy } from 'react'
 
 const meta: Meta<typeof Tags> = {
     component: Tags,
     tags: ['autodocs'],
-    decorators: (Story) => {
-        const queryClient = new QueryClient()
-        return (
-          <QueryClientProvider client={queryClient}>
-              <TagsProvider>
-                  <Story />
-              </TagsProvider>
-          </QueryClientProvider>
-        )
-    }
+    parameters: {
+        reactRouter: reactRouterParameters({
+          routing: {
+            path: '/',
+            Component: lazy(() => import('../../components/layout/index')),
+            children: [{
+                index: true,
+                loader: () => import('../../services/tagsLoader'),
+                ErrorBoundary: () => (<div>Router Error</div>),
+                Component: lazy(() => import('../../pages/tags/index'))
+            },
+            {
+                path: '*',
+                Component: lazy(() => import('../../pages/not-found/index'))
+            }]
+            
+          },
+        }),
+      },
+    decorators:[
+        withRouter,
+        (Story) => {
+            const queryClient = new QueryClient()
+            return (
+              <QueryClientProvider client={queryClient}>
+                  <TagsProvider>
+                      <Story />
+                  </TagsProvider>
+              </QueryClientProvider>
+            )
+        }
+    ]
 }
 
 export default meta
